7aced9f54cabbc2e1ae03ba682bce73723d05cc7
Tom Sorger <tomsorger314@gmail.com>
2025-09-04 14:43:36 +0200

    make tool call more dynamic

---
+++ setup/circom_civer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "[info] Installing circom_civer..."
+
+# Ensure we’re running from the script’s directory
+cd "$(dirname "$0")"
+
+
+
+
+cargo build --release
+
+
---
+++ zksec/tools/zkap.py
@@ -0,0 +1,35 @@
+import logging
+from pathlib import Path
+import os
+import subprocess, shlex
+
+
+ZKAP_DIR = Path(__file__).resolve().parent / "ZKAP"
+
+
+def execute(bug_path: str):
+    logging.debug(f"ZKAP_DIR='{ZKAP_DIR}'")
+    logging.debug(f"bug_path='{bug_path}'")
+    
+    # Verify the circuit file exists
+    circuit_file = Path(bug_path) / "circuits" / "circuit.circom"
+    if circuit_file.is_file():
+        logging.debug(f"Found circuit file: {circuit_file}")
+    else:
+        logging.warning(f"Circuit file not found: {circuit_file}")
+
+    # Change to the ZKAP directory
+    os.chdir(ZKAP_DIR)
+    logging.debug(f"Changed directory to: {Path.cwd()}")
+    # Run Circom Civer
+    # cmd = ["./run-circom-civer", str(circuit_file)]
+    # logging.debug(f"Running: {shlex.join(cmd)}")
+    # result = subprocess.run(cmd, capture_output=False, text=True, check=True)
+    result = 'not implemented'
+
+    # Change back to the original directory
+    base_dir = Path.cwd().parent.parent
+    logging.debug(f"Changing back to base directory: {base_dir}")
+    os.chdir(base_dir)
+
+    return result.stdout
