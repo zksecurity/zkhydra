80f4d4ba2484a3a56b2de013e8c031040c094263
Tom Sorger <tomsorger314@gmail.com>
2025-09-08 20:27:04 +0200

    add implementation for circom_civer

--- setup/circom_civer.sh
+++ setup/circom_civer.sh
@@ -6,9 +6,6 @@ echo "[info] Installing circom_civer..."
 # Ensure we’re running from the script’s directory
 cd "$(dirname "$0")"
 
-
-
-
 cargo build --release
 
-
+echo "[info] circom_civer installed successfully."
