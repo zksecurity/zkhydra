5b7caf8403f8b2062b9f198b639da4bebca9fede
Tom Sorger <tomsorger314@gmail.com>
2025-09-10 19:45:42 +0200

    code clean up

--- zksec/main.py
+++ zksec/main.py
@@ -1,23 +1,29 @@
 import argparse
 import logging
-import os
 import tomllib
 import importlib
 from pathlib import Path
-from bugs.zkbugs import setup as setup_zkbug
-from bugs.zkbugs import cleanup as cleanup_zkbug
+from bugs.zkbugs import setup as setup_zkbug_environment
+from bugs.zkbugs import cleanup as cleanup_zkbug_environment
 
 
 BASE_DIR = Path.cwd()
 REPO_DIR = BASE_DIR.parent
 VALID_LOG_LEVELS = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"}
-TOOL_LIST = {
-    ("circom", "circom_civer"),
-    ("circom", "circomspect",),
-    ("circom", "ecneproject",),
-    ("circom", "picus",),
-    ("circom", "zkfuzz",),
-}
+
+CAIRO = "cairo"
+CIRCOM = "circom"
+GNARK = "gnark"
+HALO2 = "halo2"
+PIL = "pil"
+
+TOOL_LIST = [
+    (CIRCOM, "circom_civer"),
+    (CIRCOM, "circomspect"),
+    (CIRCOM, "ecneproject"),
+    (CIRCOM, "picus"),
+    (CIRCOM, "zkfuzz"),
+]
 
 
 def parse_args():
@@ -47,7 +53,7 @@ def configuration(path: Path = Path("config.toml")) -> tuple[list[str], list[str
         config = tomllib.load(f)
 
     # Validate log level
-    log_level = config.get("log_level", "WARNING").upper()
+    log_level = config["app"].get("log_level", "WARNING").upper()
     if log_level not in VALID_LOG_LEVELS:
         raise ValueError(
             f"Config error: Invalid log_level '{log_level}'. "
@@ -61,51 +67,64 @@ def configuration(path: Path = Path("config.toml")) -> tuple[list[str], list[str
         datefmt="%H:%M:%S"
     )
 
-    # Validate tools & bugs
-    tools = config.get("tools", [])
-    bugs = config.get("bugs", [])
-    if not tools:
-        raise ValueError("Config error: 'tools' list must not be empty.")
-    if not bugs:
-        raise ValueError("Config error: 'bugs' list must not be empty.")
+    tools, bugs = parse_dsl_sections(config)
 
     # Ensure output directory exists
-    output_dir = Path(config.get("output", "./output"))
+    output_dir = Path(config["app"].get("output", "./output"))
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    timeout = config.get("timeout", 300)
+    timeout = config["app"].get("timeout", 300)
 
+    # make: bugs or tools::dsl::name as two vars
+    # return dsl_tools_bugs, output_dir, timeout
     return tools, bugs, output_dir, timeout
 
 
-def main():
-    args = parse_args()
-    tools, bugs, output_dir, timeout = configuration(args.config)
-
-    # Dynamically import execute functions and store in a dict
-    tool_functions = {}
-    for dsl, tool in TOOL_LIST:
-        module = importlib.import_module(f"tools.{dsl}.{tool}")
-        tool_functions[tool] = getattr(module, "execute")
-
-    for bug in bugs:
-        bug_path = REPO_DIR / bug
-        bug_name = bug_path.name
-
-        # Setup bug environment
-        setup_zkbug(bug_path)
-
-        for tool in tools:
-            tool_key = tool.lower()
-            output = Path(BASE_DIR) / output_dir / f"{dsl}" / f"{tool_key}.log"
-
-            if tool_key in tool_functions:
-                logging.info(f"Running {tool=} on {bug_name=}")
-                result = tool_functions[tool_key](bug_path, timeout)
-                write_output(output, tool_key, bug_name, result)
+def parse_dsl_sections(config: dict) -> dict[str, dict]:
+    # dsl_sections = {}
+    tools = {}
+    bugs = {}
+    for dsl, section in config.items():
+        if dsl == "app":
+            continue
+        tools[dsl] = [t.lower() for t in section.get("tools", [])]
+        bugs[dsl] = section.get("bugs", [])
         
-        # Cleanup bug environment
-        cleanup_zkbug(bug_path)
+        if not tools[dsl]:
+            raise ValueError("Config error: 'tools' list must not be empty.")
+        if not bugs[dsl]:
+            raise ValueError("Config error: 'bugs' list must not be empty.")
+    return tools, bugs
+
+
+def resolve_tools(tools: list[str]) -> dict[str, dict]:
+    loaded = {}
+    for dsl, tool in TOOL_LIST:
+        if tool not in tools: continue
+        try:
+            module = importlib.import_module(f"tools.{dsl}.{tool}")
+            loaded[tool] = {
+                "dsl": dsl,
+                "execute": getattr(module, "execute")
+            }
+        except ImportError as e:
+            logging.error(f"Failed to import {tool}: {e}")
+    return loaded
+
+
+def run_tool_on_bug(tool: str,
+                    bug_path: Path,
+                    bug_name: str,
+                    timeout: int,
+                    output_dir: Path,
+                    tool_registry: dict[str, dict]
+                    ) -> None:
+    dsl = tool_registry[tool]["dsl"]
+    execute_fn = tool_registry[tool]["execute"]
+    output = Path(BASE_DIR) / output_dir / f"{dsl}" / "raw" / f"{tool}.log"
+    logging.info(f"Running {tool=} on {bug_name=}")
+    result = execute_fn(bug_path, timeout)
+    write_output(output, tool, bug_name, result)
 
 
 def write_output(output_file: Path, tool: str, bug_name: str, content: str):
@@ -119,6 +138,26 @@ def write_output(output_file: Path, tool: str, bug_name: str, content: str):
         f.write(f"========== {bug_name} ==========\n")
         f.write(str(content))
         f.write("\n\n")
+
+
+def main():
+    args = parse_args()
+
+    tools, bugs, output_dir, timeout = configuration(args.config)
+
+    for dsl in tools:
+        tool_registry = resolve_tools(tools[dsl])
+
+        for bug in bugs[dsl]:
+            bug_path = REPO_DIR / bug
+            bug_name = bug_path.name
+
+            setup_zkbug_environment(bug_path)
+
+            for tool in tools[dsl]:
+                run_tool_on_bug(tool, bug_path, bug_name, timeout, output_dir, tool_registry)
+
+            cleanup_zkbug_environment(bug_path)
 
 
 if __name__ == "__main__":
