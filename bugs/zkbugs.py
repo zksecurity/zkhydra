35659d9a2fa75811a740bfa4423da6ac8ad45921
Tom Sorger <tomsorger314@gmail.com>
2025-10-22 19:51:31 +0200

    update config

--- bugs/zkbugs.py
+++ bugs/zkbugs.py
@@ -10,7 +10,7 @@ import sys
 from pathlib import Path
 
 BASE_DIR = Path.cwd()
-ZKBUGS_DIR = Path(__file__).resolve().parent
+ZKBUGS_DIR = Path(__file__).resolve().parent / "zkbugs"
 SCRIPT_DIR = Path(ZKBUGS_DIR) / "scripts"
 
 
